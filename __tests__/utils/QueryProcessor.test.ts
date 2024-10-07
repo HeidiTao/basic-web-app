ebfd22322463fd2fa2718d33e61d249992fcea51
HeidiTao <heidi_tao@outlook.com>
2024-10-07 11:31:24 -0400

    added processors and tests for Andrew ID queries

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,12 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return no andrew id', () => {
+        const query = "andrew id";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "I don't have one :sob:"
+        ));
+    })
 });
